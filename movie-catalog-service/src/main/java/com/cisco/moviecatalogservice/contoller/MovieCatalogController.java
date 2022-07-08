2e003fb0234f7fad5dc927ae086393efab1e13dc
cisco <sovisrushain@gmail.com>
2022-07-08 14:18:57 +0530

    use webclient to make api calls

--- movie-catalog-service/src/main/java/com/cisco/moviecatalogservice/contoller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/cisco/moviecatalogservice/contoller/MovieCatalogController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,6 +22,9 @@ public class MovieCatalogController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable String userId){
@@ -38,8 +42,15 @@ public class MovieCatalogController {
         );
 
         return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-             return new CatalogItem(movie.getName(),"DESC" , rating.getRating());
+            //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = webClientBuilder
+                    .build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+            return new CatalogItem(movie.getName(),"DESC" , rating.getRating());
         }).collect(Collectors.toList());
 
     }
