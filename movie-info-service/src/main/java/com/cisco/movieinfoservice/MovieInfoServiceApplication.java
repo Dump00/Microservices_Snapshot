7c0388514b2ee4b9377eff5259d2aedb8ffcd319
cisco <sovisrushain@gmail.com>
2022-07-10 15:25:44 +0530

    create Euraka clients

--- movie-info-service/src/main/java/com/cisco/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/cisco/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,8 +2,11 @@ package com.cisco.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
